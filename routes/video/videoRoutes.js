87f9f4ae142e097f077943985f92d2670553d3fa
Sushant <tuladharsushant01@gmail.com>
2026-06-04 15:33:26 +0800

    Added the swagger documentation for the APIs

--- routes/video/videoRoutes.js
+++ routes/video/videoRoutes.js
@@ -4,6 +4,31 @@ const { uploadVideo } = require('../../controllers/video/videoController');
 const { isAuthenticated } = require('../../middleware/auth');   
 const { uploadMiddleware } = require('../../middleware/videoUpload');
 
+
+/**
+ * @openapi
+ * /api/videos/upload:
+ *   post:
+ *     tags:
+ *       - Video
+ *     summary: Upload a video
+ *     description: Upload a video file.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               video:
+ *                type: string
+ *                format: binary
+ *     responses:
+ *       200:
+ *         description: Video uploaded successfully
+ *       400:
+ *         description: Invalid request
+ */
 router.post('/api/videos/upload', isAuthenticated, uploadMiddleware, uploadVideo);
 
 module.exports = router;
